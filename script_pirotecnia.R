9836bd41675402967d05e8c9a79af083573675e7
Jim Reed <cjoackin@gmail.com>
2021-08-31 15:15:26 -0300

    Agregue columnas de condicion con o sin pirotecnia y columna de intervalos correspondientes cada 15 minutos

--- script_pirotecnia.R
+++ script_pirotecnia.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(magrittr)
+library(purrr)
 
 tabla.datos = read.csv("./data/datosR.csv", header = TRUE, sep = ';', stringsAsFactors = TRUE)
 
@@ -11,3 +13,24 @@ a
 ## cuando es tiempo es mayor a 3600 y "sin pirotecnia" cuando es menor o igual 
 ## a 3600.
 ## usando mutate --- 
+tabla.datos
+
+datos <- tabla.datos %>%
+  mutate(condicion = case_when(
+    tiempo <= 3600 ~ 'sin_pirotecnia',
+    tiempo > 3600 ~ 'con_pirotecnia',
+  ))
+
+
+datos  %<>%  mutate(intervalo_min = case_when(
+      tiempo <= 900 ~  15,
+      tiempo <= 1800 ~ 30,
+      tiempo <= 2700 ~ 45,
+      tiempo <= 3600 ~ 60,
+      tiempo <= 4500 ~ 75,
+      tiempo <= 5400 ~ 90,
+      tiempo <= 6300 ~ 105,
+      tiempo <= 7200 ~ 120,
+    ))
+
+intervalos <- datos %>% group_by(intervalo)
